3d0e29def20973592a786f365412565332ba7137
FrigusIgnis <araujo.silva.nicolas@gmail.com>
2022-07-05 22:52:44 +0000

    beginning of the homework with MongoDB

---
+++ CRUD_Mongo_Python_BD2.py
@@ -0,0 +1,27 @@
+import pymongo
+from random import randint
+
+
+client = pymongo.MongoClient("mongodb+srv://NicolasSilva:nsda1205@sgdbpython-crud-nsda-bd.p1kei.mongodb.net/?retryWrites=true&w=majority")
+
+myDB = client["myDatabase"]
+
+collAlunos = myDB["Alunos"]
+
+
+def matricular_aluno():
+    nome = input("Insira o nome do aluno(a): ")
+    cpf = input("Insira o CPF do aluno(a): ")
+    matr = randint(10000,50000)
+    """checarMatr = collAlunos.database
+    while matr in checarMatr:
+        matr = randint(10000, 50000)"""
+    
+    matricular = {"Nome":nome, "CPF":cpf, "Matricula":matr}
+    collAlunos.insert_one(matricular)
+
+    print("Um novo aluno foi adicionado com sucesso!\n\n")
+
+
+
+matricular_aluno()
